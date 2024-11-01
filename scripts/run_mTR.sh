e899641f94f098c5634fbfce59cd563211dc240b
Ross Ellwood <rossellwood@c0709a-s30.ufhpc>
2024-11-01 13:19:42 -0400

    runmtr

--- scripts/run_mTR.sh
+++ scripts/run_mTR.sh
@@ -4,11 +4,11 @@
 #SBATCH --mail-type=FAIL,END
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=3            # Number of CPU cores per task
-#SBATCH --mem=10G
+#SBATCH --cpus-per-task=1            # Number of CPU cores per task
+#SBATCH --mem=2G
 #SBATCH --time=12:00:00
 #SBATCH --account=lien.nguyen
-#SBATCH --qos=lien.nguyen
+#SBATCH --qos=lien.nguyen-b
 
 
 
@@ -47,5 +47,5 @@
 # sed -n '1~4s/^@/>/p;2~4p' /blue/lien.nguyen/rossellwood/amplicon_pool_data_Huong/pattern_3/pool-1_1361_PK1sva_bc1011_Female_Control_17R1_43R2_p3.fastq > pool-1_1361_PK1sva_bc1011_Female_Control_17R1_43R2_p3.fasta
 # /usr/bin/time -v mTR pool-1_1361_PK1sva_bc1011_Female_Control_17R1_43R2_p3.fasta -m 0.8 > pool-1_1361_PK1sva_bc1011_Female_Control_17R1_43R2_p3_mTR_repeat_finder.txt
 
-sed -n '1~4s/^@/>/p;2~4p' /blue/lien.nguyen/rossellwood/amplicon_pool_data_Huong/pattern_3/pool-1_A15-021_PK1sva_bc1014_Male_AD_23R1_42R2_p3.fastq > pool-1_A15-021_PK1sva_bc1014_Male_AD_23R1_42R2_p3.fasta
-/usr/bin/time -v mTR pool-1_A15-021_PK1sva_bc1014_Male_AD_23R1_42R2_p3.fasta -m 0.8 > pool-1_A15-021_PK1sva_bc1014_Male_AD_23R1_42R2_p3_mTR_repeat_finder.txt
+sed -n '1~4s/^@/>/p;2~4p' /blue/lien.nguyen/rossellwood/amplicon_pool_data_Huong/demux_out/demux_fastq/demux_fastq_perSample/pool-1_A20-002_PK1sva_bc1013_Male_AD_47R1_48R2_p4.fastq > pool-1_A20-002_PK1sva_bc1013_Male_AD_47R1_48R2_p4.fasta
+/usr/bin/time -v mTR pool-1_A20-002_PK1sva_bc1013_Male_AD_47R1_48R2_p4.fasta -m 0.8 > pool-1_A20-002_PK1sva_bc1013_Male_AD_47R1_48R2_p4_mTR_repeat_finder.txt
